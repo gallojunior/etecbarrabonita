57dc1cf505d04dfdd2d24f351daf654d377b3271
GalloJunior <gallojunior@gmail.com>
2024-07-26 20:41:29 -0300

    Criação do projeto, instalação de pacotes, criação da camada de dados e serviço de conexão com banco de dados.

---
+++ EtecBarraBonita/Models/Curso.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EtecBarraBonita.Models
+{
+    [Table("Curso")]
+    public class Curso
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [Required]
+        public int EixoTecnologicoId { get; set; }
+        [ForeignKey("EixoTecnologicoId")]
+        public EixoTecnologico EixoTecnologico { get; set; }
+    }
+}
